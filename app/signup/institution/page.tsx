6356937c41492e8b9586418fc667658d9d26e08e
cowboyblurr <luminaryblur@gmail.com>
2026-07-21 10:43:20 -0400

    Use live institution signup UI in guided demo

--- app/signup/institution/page.tsx
+++ app/signup/institution/page.tsx
@@ -1,7 +1,7 @@
 import { LiveSignup } from "@/components/kleio/signup/live-signup"
-import { InstitutionOnboarding } from "@/components/kleio/signup/institution-onboarding"
+import { GuidedSignup } from "@/components/kleio/signup/guided-signup"
 import { LiveModeView } from "@/components/kleio/live-mode-view"
 
 export default function Page() {
-  return <LiveModeView live={<LiveSignup role="institution" />} preview={<InstitutionOnboarding />} />
+  return <LiveModeView live={<LiveSignup role="institution" />} preview={<GuidedSignup role="institution" />} />
 }
