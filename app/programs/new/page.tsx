912bd41375a3ebf8cace2dfe5a16019dee949359
cowboyblurr <luminaryblur@gmail.com>
2026-07-22 19:33:15 -0400

    Place Cleo guidance in open call creation

--- app/programs/new/page.tsx
+++ app/programs/new/page.tsx
@@ -1,12 +1,20 @@
 import { DashboardShell } from "@/components/kleio/dashboard-shell"
 import { ProgramsNewPageView } from "@/components/kleio/programs-new-page-view"
+import { InstitutionCleoStatusBar } from "@/components/kleio/institution-cleo-status-bar"
 import { LiveModeView } from "@/components/kleio/live-mode-view"
 import { LiveInstitutionCallsWithImages } from "@/components/kleio/live-institution-calls-with-images"
 
 export default function Page() {
   return (
     <DashboardShell>
-      <LiveModeView live={<LiveInstitutionCallsWithImages />} preview={<ProgramsNewPageView />} />
+      <div className="flex h-full min-h-0 flex-col bg-white">
+        <div className="shrink-0 px-4 pt-4 sm:px-6">
+          <InstitutionCleoStatusBar />
+        </div>
+        <div className="min-h-0 flex-1">
+          <LiveModeView live={<LiveInstitutionCallsWithImages />} preview={<ProgramsNewPageView />} />
+        </div>
+      </div>
     </DashboardShell>
   )
 }
