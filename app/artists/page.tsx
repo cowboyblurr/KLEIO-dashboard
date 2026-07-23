4acb444017f769cddbf3a01877486ace2df039d1
cowboyblurr <luminaryblur@gmail.com>
2026-07-23 11:10:43 -0400

    Route institutions to opt-in artist discovery

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,12 +1,12 @@
 import { DashboardShell } from "@/components/kleio/dashboard-shell"
 import { ArtistsDirectory } from "@/components/kleio/artists-directory"
-import { LiveInstitutionArtists } from "@/components/kleio/live-institution-artists"
+import { LiveArtistDiscovery } from "@/components/kleio/live-artist-discovery"
 import { LiveModeView } from "@/components/kleio/live-mode-view"
 
 export default function Page() {
   return (
     <DashboardShell>
-      <LiveModeView live={<LiveInstitutionArtists />} preview={<ArtistsDirectory />} />
+      <LiveModeView live={<LiveArtistDiscovery />} preview={<ArtistsDirectory />} />
     </DashboardShell>
   )
 }
