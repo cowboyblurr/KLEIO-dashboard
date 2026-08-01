e4c735045df5df93ab021183ab42bb170bb797ac
cowboyblurr <luminaryblur@gmail.com>
2026-08-01 09:32:43 -0400

    Align institution-type constraint with onboarding options

--- docs/migrations/20260801_guided_onboarding_preferences.sql
+++ docs/migrations/20260801_guided_onboarding_preferences.sql
@@ -9,3 +9,28 @@ comment on column public.artist_profiles.onboarding_preferences is
 
 comment on column public.institutions.onboarding_preferences is
   'Institution-selected onboarding preferences used to personalize initial workspace recommendations.';
+
+alter table public.institutions
+  drop constraint if exists institutions_organization_type_check;
+
+alter table public.institutions
+  add constraint institutions_organization_type_check
+  check (
+    organization_type = any (
+      array[
+        'museum'::text,
+        'gallery'::text,
+        'arts_nonprofit'::text,
+        'foundation'::text,
+        'residency'::text,
+        'university_college'::text,
+        'cultural_organization'::text,
+        'government_arts_agency'::text,
+        'independent_curatorial_organization'::text,
+        'grantmaking_organization'::text,
+        'festival_biennial'::text,
+        'artist_run_organization'::text,
+        'other'::text
+      ]
+    )
+  );
